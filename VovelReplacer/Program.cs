fa06bc7c80e6f0e6c644885b932882b82f856b59
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-10-31 13:26:02 +0100

    Add project files.

---
+++ VovelReplacer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VovelReplacer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program replaces all the vowels in a string with a specified character.");
+            
+            Console.WriteLine("Enter a string:");
+            string userString = Console.ReadLine();
+
+            var vovelReplacer = new Replacer();
+            vovelReplacer.Replace(userString);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VovelReplacer/Replacer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VovelReplacer
+{
+    class Replacer
+    {
+        private List<string> vovels = new List<string> { "a", "e", "i", "o", "u", "y" };
+
+        public void Replace (string userString)
+        { 
+            char substitute = '#';
+
+            for (int i =0; i<userString.Length; i++)
+            {
+                for(int j =0; j<vovels.Count; j++)
+                if (userString.Contains(vovels[j]))
+                {
+                     userString.Replace(userString[i], substitute);                        
+                }
+            }
+            Console.WriteLine(userString);
+        }
+    }
+}
