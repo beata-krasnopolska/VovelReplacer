f5cda3487454db9d5ffe6ec90f638831ecfabb3e
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-10-31 16:59:21 +0100

    Add foreach loop

--- VovelReplacer/Replacer.cs
+++ VovelReplacer/Replacer.cs
@@ -5,20 +5,19 @@ namespace VovelReplacer
 {
     class Replacer
     {
-        private List<string> vovels = new List<string> { "a", "e", "i", "o", "u", "y" };
+        private List<string> _vovels = new List<string> { "a", "e", "i", "o", "u", "y" };
+        private const string _substitute = "#";
 
         public void Replace (string userString)
-        { 
-            char substitute = '#';
-
-            for (int i =0; i<userString.Length; i++)
+        {
+            foreach (var vovel in _vovels)
             {
-                for(int j =0; j<vovels.Count; j++)
-                if (userString.Contains(vovels[j]))
+                if (userString.Contains(vovel))
                 {
-                     userString.Replace(userString[i], substitute);                        
+                    userString = userString.Replace(vovel, _substitute);
                 }
             }
+
             Console.WriteLine(userString);
         }
     }
